6f866dab7f5647df46795965be8b863120fe5d01
Kymemia <kimemiachristopherr@gmail.com>
2024-08-20 16:19:55 +0300

    Refactor: all methods from __init__ to insert method (from MySQL to SQLAlchemy)

--- models/engine/storage.py
+++ models/engine/storage.py
@@ -5,12 +5,19 @@ This is the storage model for Kitsune,
 containing every model and attribute and method
 that will be used for the site
 """
-from typing import List, Union, Tuple
+from sqlalchemy import create_engine, Table, MetaData, Update
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import URL
+from sqlalchemy.sql import text
+from sqlalchemy.exc import SQLAlchemyError
+from typing import List, Union, Tuple, Dict
 import mysql.connector
 from mysql.connector import Error
 
+# Database url + creation of SQLAlchemy engine + Configured session class + creation of session instance
 
-class MySQLStorage:
+
+class Storage:
     """
     class definition that contains
     all the models and attributes
@@ -19,13 +26,13 @@ class MySQLStorage:
     def __init__(
             self, host: str, user: str,
             password: str, database: str,
-            port=3306
+            port=3306, drivername:str = 'mysql+pymysql'
             ) -> None:
         """
         method definition to initialize all the attributes
         of the class as listed below:
 
-        **PARAMETERS**
+        Args:
         host => This is the the IP address of the MySQL server.
         user => This is the username for authenticating the database.
         password => This will be used to authenticate
@@ -36,13 +43,19 @@ class MySQLStorage:
         Example
         ---------
         """
-        self._host = host
-        self._user = user
-        self._password = password
-        self._database = database
-        self._port = port
-        self._connection = None
-        self._cursor = None
+        db_config = {
+                'drivername': driver,
+                'username': user,
+                'password': password,
+                'host': host,
+                'port': port,
+                'database': database
+                }
+
+        self._uri = URL.create(**db_config)
+        self._engine = create_engine(self._uri)
+        self._SessionFactory = sessionmaker(bind=self._engine)
+        self._session = self._SessionFactory
 
     def connect(self) -> None:
         """
@@ -60,139 +73,175 @@ class MySQLStorage:
         Else, returns the error caught during the connection process.
         """
         try:
-            self._connection = mysql.connector.connect(
-                    host=self._host,
-                    user=self._user,
-                    password=self._password,
-                    database=self._database,
-                    port=self._port
-                    )
-            self._cursor = self._connection.cursor(dictionary=True)
-            print("Success! MySQL connected.")
-        except Error as e:
-            print(f"Error connecting to MySQL: {e}")
+            self._engine = create_engine(self._uri)
+
+            SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
+            self._session = SessionLocal()
+
+            print("Success! SQLAlchemy connected.")
+        except SQLAlchemyError as e:
+            print(f"Error connecting to the database: {e}")
             raise
 
     def disconnect(self) -> None:
         """
-        This be a method definition to disconnect the connection to MySQL.
-        It checks if the cursor exists and is open, then closes it.
-        It also checks if the connection exists and is open,
-        then closes it as well.
+        This be a method definition to disconnect the connection to SQLAlchemy.
+        It also checks if the connection exists, then closes it.
         """
-        if self._cursor:
-            self._cursor.close()
-        if self._connection:
-            self._connection.close()
-            print("MySQL connection closed")
+        if self._session:
+            try:
+                if self._session.is_active:
+                    self._session.commit()
+                print("Pending transactions committed")
+            except Exception as e:
+                self._session.rollback()
+                print(f"Error occured >> {e}. Transactions rolled back.")
+            finally:
+                self._session.close()
+                print("SQLAlchemy connection closed")
 
     def execute_query(self, query: str, params: Tuple[str] = None) -> int:
         """
-        This method definition executes an SQL query based
-        on the query given by the user and parameters
+        This method definition executes an SQL using SQLAlchemy's interface
 
-        **PARAMETERS**
+        Args:
         query => this is the SQL query that'll be executed
         params => this is a tuple of parameters that'll be used
                     in the SQL query (E.g: username/email)
 
-        Returns the number of rows affected by the error on success.
-        Else, returns the caught error.
+        Returns:
+            int: The number of rows affected by the error on success.
+        Raises:
+            Exception: Returns the caught error.
+
+        Example:
+            To update a user's email in our database:
+
+            query = "UPDATE users SET email = :email
+                    WHERE username = :username"
         """
         try:
-            self._cursor.execute(query, params)
-            self._connection.commit()
-            return self._cursor.rowcount
-        except Error:
+            query_statement = text(query)
+            result = self._session.execute(query_statement, params)
+            self._session.commit()
+            return result.rowcount
+        except SQLAlchemyError:
             print("Error executing error: {e}")
-            self.rollback_transaction()
+            self._session.rollback()
             raise
 
     def fetch_one(
             self, query: str,
             params: Tuple[str] = None
-            ) -> Union[dict, tuple]:
+            ) -> Union[Dict, Tuple]:
         """
-        This method definition executes a SQL query and fetches
-        only a single record from a certain result set.
+        This method definition executes an SQL query using SQLAlchemy
+        and fetches only a single record from a certain result set.
         (E.g.: primary key or unique user_id)
 
-        **PARAMETERS**
-        query => This is the SQL query that will be executed (string).
+        Args:
+        query => This is the SQL query that will be executed (str).
         params => This is a tuple of parameters
-                    that'll be used in the SQL query.
+                    that'll be used in the SQL query
+                        (Tuple[str], optional).
 
-        Returns a single record from the result set,
-        either in dict or tuple, on success.
-        Else, returns error caught.
+        Returns:
+            A single record from the result set,
+            either in dict or tuple, on success.
+        
+        Raises:
+            SQLAlchemyError caught during execution.
+            
+        Example:
+            query = "SELECT * FROM users WHERE username = :username"
         """
         try:
-            self._cursor.execute(query, params)
-            return self._cursor.fetchone()
-        except Error as e:
+            query_statement = text(query)
+            result = self._session.execute(query_statement, params)
+            record = result.fetchone()
+            self._session.commit()
+            return record
+        except SQLAlchemyError as e:
             print(f"Error fetching record: {e}")
+            self._session.rollback()
             raise
 
     def fetch_all(
             self, query: str,
             params: Tuple[str] = None
-            ) -> List[Union[dict, tuple]]:
+            ) -> List[Union[Dict, Tuple]]:
         """
-        This method definition executes a SQL query
+        This method definition executes a SQL query using SQLAlchemy
         and fetches all the records from the result set.
             (E.g: Fetching all users from the Users table)
 
-        **PARAMETERS**
-        query => This is the SQL query to be executed.
-        params => This is a tuple of parameters
-                    to be used in the SQL query.
+        Args:
+            query => This is the SQL query to be executed.
+            params => This is a tuple of parameters
+                        to be used in the SQL query.
 
-        Returns a list of records from the result set on success.
-        Else, returns error caught.
+        Returns:
+            List[Union[Dict, Tuple]]: A list of records
+                                    from the result set on success.
+        Raise:
+            SQLAlchemy error caught during query execution.
+
+        Example:
+            query = "SELECT * FROM users WHERE status = :status"
         """
         try:
-            self._cursor.execute(query, params)
-            return self._cursor.fetchall()
-        except Error as e:
+            query_statement = text(query)
+            result = self._session.execute(query_statement, params)
+            records = result.fetchall()
+            self._session.commit()
+            return records
+        except SQLAlchemyError as e:
             print(f"Error fetching records: {e}")
+            self._session.rollback()
             raise
 
-    def insert(self, table: str, data: dict) -> int:
+    def insert(self, table: str, data: Dict[str, any]) -> int:
         """
         This method definition inserts new data
         into a specified table based on the provided data.
-        It creates a string of placeholders for the SQL query based
-        on the number of data items then creates
-        a string of column names for the query itself.
 
-        **PARAMETERS**
-        table => This is the name of the table
-                    where the record will be inserted.
-        data => This is a dictionary where keys == column names
-                    and values are the corresponding values
-                        that will be inserted.
+        Args:
+            table(str) => This is the name of the table
+                        where the record will be inserted.
+            data (Dict[str, any]) => This is a dictionary where keys == column names
+                        and values are the corresponding values
+                            that will be inserted.
 
-        Returns the Id of the inserted data on success.
-        Else, returns caught error.
+        Returns:
+            int: The Id of the inserted data on success.
+
+        Raises:
+            SQLAlchemy error if there was an error durinf execution.
+
+        Example:
+            table = 'users'
+            data = {'username': 'senor_mcmuffins', 'email': 'senor@mcmuffins.com'}
+            inserted_id = self.insert(table, data)
         """
-        placeholders = ', '.join(['%s'] * len(data))
-        columns = ', '.join(data.keys())
-        query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
         try:
-            self._cursor.execute(query, list(data.values()))
-            self._connection.commit()
-            return self._cursor.lastrowid
-        except Error as e:
+            metadata = MetaData(bind=self._engine)
+            target_table = Table(table, metadata, autoload_with=self._engine)
+            query_statement = target_table.insert().values(data)
+            result = self._session.execute(query_statement)
+            self._session.commit()
+            return result.inserted_primary_key[0] if result.inserted_primary_key else None
+        except SQLAlchemyError as e:
             print(f"Error inserting data: {e}")
+            self._session.rollback()
             raise
 
-    def update(self, table: str, data: dict, condition: str) -> int:
+    def update(self, table: str, data: Dict[str, any], condition: str) -> int:
         """
         This method definition updates records
         of a specified table based on the provided data
         and custom conditions specified by the user.
 
-        **PARAMETERS**
+        Args:
         table => This is the name of the table that'll be updated.
         data => This is a dictionary where keys == column names
                     and values  == new values to be set.
@@ -217,9 +266,11 @@ class MySQLStorage:
         This method definition deletes items from a specified table
         based on a user's custom condition.
 
-        **PARAMETERS**
-        table => This is the table name from which items will be deleted.
-        condition => This is the condition to specify which items to delete.
+        Args:
+            table => This is the table name
+            from which items will be deleted.
+            condition => This is the condition
+            to specify which items to delete.
 
         Returns number of rows affected on success.
         Else, returns caught error.
@@ -281,7 +332,7 @@ class MySQLStorage:
         Once the table is created with a query,
         the transaction is committed to make the creation permanent.
 
-        **PARAMETERS**
+        Args:
         table_name => This is the name that'll be assigned
                         to the table created.
         columns => This will be the number of columns assigned
@@ -312,7 +363,7 @@ class MySQLStorage:
         After doing so, the transaction is committed
         to make it permanent.
 
-        **PARAMETERS**
+        Args:
         table_name => This will be the name
                         of the table to be used for this operation.
 
