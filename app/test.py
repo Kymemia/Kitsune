76396951ec630205e510e055e019feee07bd43f5
Kymemia <kimemiachristopherr@gmail.com>
2024-10-08 12:34:09 +0300

    Added: test.py for testing flask

--- app/test.py
+++ app/test.py
@@ -12,5 +12,5 @@ def index():
     return 'Welcome to Gig City'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
